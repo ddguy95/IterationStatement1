c7e3a2f40861f7cbde5cb4e7949c77b3d1a3633e
derrickdrye <dryd95@hotmail.com>
2023-06-23 11:14:52 -0500

    IterationStatement1LoopComplete

---
+++ Loop15Times.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i > 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i> 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i > 5; i++)
+{
+    Console.Writeline($"{i}")
+}
+
+for (int i = 0; i > 5; i++)
+{
+    Console.Writeline($"{1}")
+}
+
+for (int i = 0; i > 5; i++)
+{
+    Console.Writeline($"{1}")
+}
